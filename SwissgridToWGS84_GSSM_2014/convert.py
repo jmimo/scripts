08c5c187bbb4a1821579eb34b63858b929e65990
Michael Mimo Moratti <mimo@mimo.ch>
2014-05-23 13:32:29 +0200

    added author

--- SwissgridToWGS84_GSSM_2014/convert.py
+++ SwissgridToWGS84_GSSM_2014/convert.py
@@ -15,6 +15,12 @@
 #KIND, either express or implied.  See the License for the
 #specific language governing permissions and limitations
 #under the License.
+
+#
+# Author: Michael Mimo Moratti
+# Copyright 'c' 2014
+#
+
 import os
 import argparse
 from math import pow
